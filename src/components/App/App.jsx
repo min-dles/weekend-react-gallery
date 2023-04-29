7704a8ce71d19f0e025c34c083855f4181b1ab53
mandi andrade <mandi.andrade20@gmail.com>
2023-04-28 20:59:18 -0500

    added img useEffect and axios fetch

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,14 +1,32 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import './App.css';
+import axios from 'axios';
 
 function App() {
+
+  useEffect(() => {
+    fetchImages();
+  }, [])
+
+  const fetchImages = () => {
+    axios({
+      method: 'GET',
+      url:'/gallery'
+    }).then((response) => {
+      const theImages = response.data;
+      console.log('response and Imgs:', response, theImages);
+    }).catch((error) => {
+      console.log('error in axios GET', error);
+    })
+  }
     return (
       <div className="App">
         <header className="App-header">
           <h1 className="App-title">Gallery of My Life</h1>
         </header>
         <p>Gallery goes here</p>
-        <img src="images/goat_small.jpg"/>
+        {/* <img src="images/goat_small.jpg"/>
+        <img src="images/lola_small.jpg"/> */}
       </div>
     );
 }
