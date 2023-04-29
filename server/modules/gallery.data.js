fbdd808c0291fca3263748732b8a0baa7402fc6a
mandi andrade <mandi.andrade20@gmail.com>
2023-04-28 20:59:24 -0500

    added image, axios fetch, and useEffect

--- server/modules/gallery.data.js
+++ server/modules/gallery.data.js
@@ -1,5 +1,16 @@
 const galleryItems = [
-    { id: 1, path: 'images/goat_small.jpg', description: 'Photo of a goat taken at Glacier National Park.', likes: 0 }
+    { 
+        id: 1, 
+        path: 'images/goat_small.jpg', 
+        description: 'Photo of a goat taken at Glacier National Park.', 
+        likes: 0 
+    },
+    { 
+        id: 2, 
+        path: 'images/lola_small.jpg',
+        description: 'Photo of a goat taken at Minneahaha Dog Park.',
+        likes: 1000000000
+    }
 ];
 
 module.exports = galleryItems;
