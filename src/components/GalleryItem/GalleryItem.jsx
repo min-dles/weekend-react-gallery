01dd6b646389c5ecb57c7dc24e0171ed517754b6
mandi andrade <mandi.andrade20@gmail.com>
2023-04-29 12:41:10 -0500

    successful toggle img and desc tests

--- src/components/GalleryItem/GalleryItem.jsx
+++ src/components/GalleryItem/GalleryItem.jsx
@@ -1,4 +1,4 @@
-import {useState} from 'react';
+import { useState } from 'react';
 
 function GalleryItem(props) {
     // I want image to be displayed first- so setting state to true:
@@ -6,22 +6,36 @@ function GalleryItem(props) {
 
     // going to toggle image and description here:
     const toggleDisplay = () => {
-        console.log('Image was clicked!');
+        // console.log('Image was clicked!');
         if (displayImage) {
-                setDisplayImage(false)
-            } else {
-                    setDisplayImage(true)
-                }
+            setDisplayImage(false)
+        } else {
+            setDisplayImage(true)
+        }
+    }
+
+    // make a component to change out the HTML: 
+    const ImageOrDescription = () => {
+        if (displayImage) {
+            return (
+                <img
+                    src={props.image.path}
+                    onClick={toggleDisplay}
+                />
+            )
+        } else {
+            return (
+                <p onClick={toggleDisplay}>
+                    image description: {props.image.description}
+                </p>
+            )
+        }
     }
 
     return (
         <li>
-            TEST: displayImage is {displayImage ? "true" : "false"}
-            <img 
-                src={props.image.path} 
-                onClick={toggleDisplay}
-            />
-            <p>image description: {props.image.description}</p>
+            {/* TEST: displayImage is {displayImage ? "true" : "false"} */}
+            <ImageOrDescription />
         </li>
     )
 }
