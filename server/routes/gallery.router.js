ae681bcaa0bcc317301e23d3e860741505c6e432
mandi andrade <mandi.andrade20@gmail.com>
2023-04-30 18:10:40 -0500

    editing PUT route, need to move +1 func

--- server/routes/gallery.router.js
+++ server/routes/gallery.router.js
@@ -6,14 +6,30 @@ const pool = require('../modules/pool.js');
 
 // PUT Route
 router.put('/like/:id', (req, res) => {
-    console.log(req.params);
+    console.log(req.params.id);
+    const sqlText = `
+        UPDATE images
+            SET "likes"=$1
+            WHERE "id"=$2;
+    `;
+
     const galleryId = req.params.id;
+    const galleryItems = req.params;
     for (const galleryItem of galleryItems) {
         if (galleryItem.id == galleryId) {
             galleryItem.likes += 1;
         }
+        const sqlValues = [galleryItem.likes, galleryId];
+        
+        pool.query(sqlText, sqlValues)
+        .then((dbRes) => {
+            console.log('DB updated likes!', dbRes);
+            res.sendStatus(200);
+        }).catch((dbErr) => {
+            console.log('Error updating DB likes', dbErr);
+            res.sendStatus(500);
+        })
     }
-    res.sendStatus(200);
 }); // END PUT Route
 
 // GET Route
@@ -26,8 +42,8 @@ router.get('/', (req, res) => {
     pool.query(sqlText)
         .then((result) => {
             res.send(result.rows);
-        }).catch((error) => {
-            console.log('GET /gallery fail:', error);
+        }).catch((err) => {
+            console.log('GET /gallery fail:', err);
             res.sendStatus(500);
         })
 }); // END GET Route
